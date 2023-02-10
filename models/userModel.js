e138d17005be6a4648ade56e4c69a75c0d0d7bb8
Asaf Kave <kaveasaf@gmail.comm>
2023-02-10 18:54:00 +0200

    After 10.142

--- models/userModel.js
+++ models/userModel.js
@@ -43,7 +43,12 @@ const userSchema = new mongoose.Schema({
   },
   passwordChangedAt: Date,
   passwordResetToken: String,
-  passwordResetExpires: Date
+  passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false
+  }
 });
 
 userSchema.pre('save', async function(next) {
@@ -62,6 +67,13 @@ userSchema.pre('save', async function(next) {
   if (!this.isModified('password') || this.isNew) return next();
 
   this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
+userSchema.pre('/^find/', async function(next) {
+  // this points to the current query
+  console.log(this);
+  this.find({ active: { $ne: false } });
   next();
 });
 
