85f91c0c6c99ef7ad4903edd58a0ef061c3c0bac
Asaf Kave <kaveasaf@gmail.comm>
2023-02-12 13:32:18 +0200

    After 10.159

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
 const authController = require('./../controllers/authController');
-const reviewController = require('./../controllers/reviewController');
+const reviewRouter = require('./../routes/reviewRoutes');
 
 const router = express.Router();
+
+// POST /tour/234faac4/reviews
+// GET /tour/234faac4/reviews
+// GET /tour/234faac4/reviews/24323ac4
+
+router.use('/:tourId/reviews', reviewRouter);
 
 // router.param('id', tourController.checkID);
 
@@ -27,18 +33,6 @@ router
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
     tourController.deleteTour
-  );
-
-// POST /tour/234faac4/reviews
-// GET /tour/234faac4/reviews
-// GET /tour/234faac4/reviews/24323ac4
-
-router
-  .route('/:tourId/reviews')
-  .post(
-    authController.protect,
-    authController.restrictTo('user'),
-    reviewController.createReview
   );
 
 module.exports = router;
